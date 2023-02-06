ccfd16f8bcede08bfd0309629ca911f7aa1d8c00
Anonymous <>
2023-02-07 01:27:33 +0900

    fix pip

--- src/main/pip.ts
+++ src/main/pip.ts
@@ -2,8 +2,26 @@ import { BrowserWindow } from "electron";
 import createWindow from "./window";
 
 export function createPIP() {
-  const pip = createWindow(undefined, "index.html/pip");
-  const control = createWindow(undefined, "index.html/control");
+  const pip = createWindow({
+    width: 640,
+    height: 360,
+    frame: false,
+  });
+  const control = createWindow({
+    width: 640,
+    height: 120,
+    frame: false,
+  });
+  pip.setAspectRatio(16 / 9);
+
+  function syncControl() {
+    const [x, y] = pip.getPosition();
+    control.setPosition(x, y + pip.getSize()[1]);
+    control.setSize(pip.getSize()[0], 120);
+  }
+
+  pip.on('resize', syncControl);
+  pip.on('move', syncControl);
 
   pip.on("closed", () => {
     control?.close();
