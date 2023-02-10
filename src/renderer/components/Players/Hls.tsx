f534f66ba1ebf5b2e3f412cf7412a763b8ac1b91
Anonymous <>
2023-02-10 23:13:42 +0900

    add twitch & hls player

---
+++ src/renderer/components/Players/Hls.tsx
@@ -0,0 +1,64 @@
+import Hls from 'hls.js';
+import { useEffect } from 'react';
+
+interface PropType {
+  source?: string;
+  autoPlay?: boolean;
+  paused?: boolean;
+  volume?: number;
+  currentTime?: number;
+  onCurrentTimeUpdate?: (time: number) => any;
+  onDurationChange?: (duration: number) => any;
+}
+
+function Player(props: PropType) {
+  useEffect(() => {
+    const video = document.querySelector('video');
+    if (video) {
+      const hls = new Hls();
+      hls.loadSource(props.source as string);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        if (props.autoPlay) video.play();
+      });
+    }
+  }, [props.source]);
+  useEffect(() => {
+    const video = document.querySelector('video');
+    if (video) {
+      if (props.paused) {
+        video.pause();
+      } else {
+        video.play()?.catch(() => void 0);
+      }
+    }
+  }, [props.paused]);
+  useEffect(() => {
+    const video = document.querySelector('video');
+    if (video && props.currentTime) {
+      video.currentTime = props.currentTime;
+    }
+  }, [props.currentTime]);
+  useEffect(() => {
+    const video = document.querySelector('video');
+    if (video && props.volume) {
+      video.volume = props.volume;
+    }
+  }, [props.volume]);
+
+  return (
+    <video
+      autoPlay={props.autoPlay}
+      onTimeUpdate={(event) => {
+        props.onCurrentTimeUpdate?.(
+          (event.target as HTMLVideoElement).currentTime
+        );
+      }}
+      onDurationChange={(event) => {
+        props.onDurationChange?.((event.target as HTMLVideoElement).duration);
+      }}
+    />
+  );
+}
+
+export default Player;
